02714b1d6c4fe676c5e35ee9990d0efcafe50692
123 <123@DESKTOP-6J147SD>
2022-11-10 23:17:46 +0200

    Добавьте файлы проекта.

---
+++ 2tochka3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+
+        static void main(string[] args)
+        {
+            int a = -3;
+            int b = 3.5;
+
+            if (a * b > 0)
+            {
+                a = -a;
+                b = -b;
+            }
+            else
+            {
+                a = b = 0;
+            }
+
+            Console.WriteLine($"{a} {b}");
+
+            Console.ReadLine();
+        }
+    }
+}
